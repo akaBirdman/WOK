11eab2e03a140321dd4734c4c33f1d2b9d227dea
Caleb Todd <82277669+akaBirdman@users.noreply.github.com>
2023-11-09 15:33:06 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,40 +1,54 @@
-// Progress Bar
-const stops = document.querySelectorAll('section.progress-roadmap > .journey > .stop');
-const descs = document.querySelectorAll('section.progress-roadmap > .descriptions > p');
-const roads = document.querySelectorAll('section.progress-roadmap > .journey > .road');
+// // Progress Bar
+// const stops = document.querySelectorAll('section.progress-roadmap > .journey > .stop');
+// const descs = document.querySelectorAll('section.progress-roadmap > .descriptions > p');
+// const roads = document.querySelectorAll('section.progress-roadmap > .journey > .road');
 
-const checkpoint = 1;
+// const checkpoint = 0;
 
-for (let i = 0; i <= checkpoint; i++) {
-    stops[i].classList.add('complete');
-    
-    if (i <= 0) continue;
-    roads[i - 1].classList.add('complete');
-}
+// for (let i = 0; i <= checkpoint; i++) {
+//     stops[i].classList.add('complete');
 
-stops[checkpoint].classList.add('current');
-descs[checkpoint].classList.add('complete');
+//     if (i <= 0) continue;
+//     roads[i - 1].classList.add('complete');
+// }
 
-try {
-    roads[checkpoint].classList.add('in-progress');
-    stops[checkpoint + 1].classList.add('in-progress');
-} catch(error) {}
+// stops[checkpoint].classList.add('current');
+// descs[checkpoint].classList.add('current');
+// descs[checkpoint].classList.add('complete');
+
+// try {
+//     roads[checkpoint].classList.add('in-progress');
+//     stops[checkpoint + 1].classList.add('in-progress');
+//     descs[checkpoint + 1].classList.add('in-progress');
+// } catch(error) {}
 
 
-// Visual
-const visual = document.querySelector('section.progress-roadmap > .visual > svg');
-const visual_viewbox = visual.viewBox.baseVal;
-for (let y = 0; y < visual_viewbox.height; y++) {
-    for (let x = 0; x < visual_viewbox.width; x++) {
-        const rect = document.createElementNS("http://www.w3.org/2000/svg", 'rect');
-        rect.setAttribute('width', '1');
-        rect.setAttribute('height', '1');
-        rect.setAttribute('x', x);
-        rect.setAttribute('y', y);
-        rect.onclick = (event) => {
-            console.log(event);
-            event.target.classList.toggle('active');
-        }
-        visual.appendChild(rect)
-    }
+// // Visual
+// const visual = document.querySelector('section.progress-roadmap > .visual > svg');
+// const visual_viewbox = visual.viewBox.baseVal;
+// for (let y = 0; y < visual_viewbox.height; y++) {
+//     for (let x = 0; x < visual_viewbox.width; x++) {
+//         const rect = document.createElementNS("http://www.w3.org/2000/svg", 'rect');
+//         rect.setAttribute('width', '1');
+//         rect.setAttribute('height', '1');
+//         rect.setAttribute('x', x);
+//         rect.setAttribute('y', y);
+//         rect.onclick = (event) => {
+//             console.log(event);
+//             event.target.classList.toggle('active');
+//         }
+//         visual.appendChild(rect)
+//     }
+// }
+
+function toggle_active(element) {
+    element.blur();
+    element.closest('.fry').querySelector('.accordion').classList.toggle('active');
+
+    // const stir_fries = document.querySelectorAll('.stir > .fry');
+    // for (const meal of stir_fries) {
+    //     try {
+    //         meal.querySelector('.accordion').classList.remove('active');
+    //     } catch(error) {}
+    // }
 }
