1244274fc3b71127e4d37c38f1fe8ce367506ebf
akaBirdman <cbonetodd@gmail.com>
2023-11-28 12:44:26 -0600

    Image Slider and General Fixes

--- main.js
+++ main.js
@@ -1,54 +1,50 @@
-// // Progress Bar
-// const stops = document.querySelectorAll('section.progress-roadmap > .journey > .stop');
-// const descs = document.querySelectorAll('section.progress-roadmap > .descriptions > p');
-// const roads = document.querySelectorAll('section.progress-roadmap > .journey > .road');
-
-// const checkpoint = 0;
-
-// for (let i = 0; i <= checkpoint; i++) {
-//     stops[i].classList.add('complete');
-
-//     if (i <= 0) continue;
-//     roads[i - 1].classList.add('complete');
-// }
-
-// stops[checkpoint].classList.add('current');
-// descs[checkpoint].classList.add('current');
-// descs[checkpoint].classList.add('complete');
-
-// try {
-//     roads[checkpoint].classList.add('in-progress');
-//     stops[checkpoint + 1].classList.add('in-progress');
-//     descs[checkpoint + 1].classList.add('in-progress');
-// } catch(error) {}
-
-
-// // Visual
-// const visual = document.querySelector('section.progress-roadmap > .visual > svg');
-// const visual_viewbox = visual.viewBox.baseVal;
-// for (let y = 0; y < visual_viewbox.height; y++) {
-//     for (let x = 0; x < visual_viewbox.width; x++) {
-//         const rect = document.createElementNS("http://www.w3.org/2000/svg", 'rect');
-//         rect.setAttribute('width', '1');
-//         rect.setAttribute('height', '1');
-//         rect.setAttribute('x', x);
-//         rect.setAttribute('y', y);
-//         rect.onclick = (event) => {
-//             console.log(event);
-//             event.target.classList.toggle('active');
-//         }
-//         visual.appendChild(rect)
-//     }
-// }
+const ingredients = document.querySelectorAll('#navigation .stir > .fry > .ingredients');
+ingredients.forEach(e => {
+    e.removeAttribute('href');
+    e.onclick = event => { toggle_active(event.target) }
+})
 
 function toggle_active(element) {
+    const navs = document.querySelectorAll('nav .stir > .fry > a');
+    navs.forEach((e) => {
+        if (e != element) {
+            try {
+                e.closest('.fry')
+                .querySelector('.accordion')
+                .classList.remove('active');
+            } catch(error) {}
+        }
+    });
+
     element.blur();
     element.closest('.fry').querySelector('.accordion').classList.toggle('active');
+}
 
-    // const stir_fries = document.querySelectorAll('.stir > .fry');
-    // for (const meal of stir_fries) {
-    //     try {
-    //         meal.querySelector('.accordion').classList.remove('active');
-    //     } catch(error) {}
-    // }
+// Auto Slideshow
+function do_scroll() { // Delete this function later
+    const slideShow = document.querySelector('#welcome > .carousel > .slideshow');
+    slideshow_scrollNext(slideShow);
+}
+
+slideshow_appendAll();
+
+function slideshow_appendAll() {
+    const slideShows = document.querySelectorAll('.slideshow');
+    slideShows.forEach(element => {
+        firstChild = element.children[0];
+        newChild = firstChild.cloneNode(true);
+        element.appendChild(newChild);
+    });
+}
+
+function slideshow_scrollNext(element) {
+    const scrollAmmount = element.clientWidth;
+    element.setAttribute('slideIndex', parseInt(element.getAttribute('slideIndex')) + 1);
+    let slideIndex = element.getAttribute('slideIndex');
+    if (slideIndex == element.children.length) {
+        element.scrollTo({ top:0, left:0, behavior: "instant"})
+        element.setAttribute('slideIndex', 1);
+        slideIndex = 1;
+    }
+    element.scrollLeft = slideIndex * scrollAmmount;
 }
---
+++ progress.js
@@ -0,0 +1,41 @@
+// Progress Bar
+const stops = document.querySelectorAll('section.progress-roadmap > .journey > .stop');
+const descs = document.querySelectorAll('section.progress-roadmap > .descriptions > p');
+const roads = document.querySelectorAll('section.progress-roadmap > .journey > .road');
+
+const checkpoint = 1
+;
+
+for (let i = 0; i <= checkpoint; i++) {
+    stops[i].classList.add('complete');
+    
+    if (i <= 0) continue;
+    roads[i - 1].classList.add('complete');
+}
+
+stops[checkpoint].classList.add('current');
+descs[checkpoint].classList.add('complete');
+
+try {
+    roads[checkpoint].classList.add('in-progress');
+    stops[checkpoint + 1].classList.add('in-progress');
+} catch(error) {}
+
+
+// Visual
+const visual = document.querySelector('section.progress-roadmap > .visual > svg');
+const visual_viewbox = visual.viewBox.baseVal;
+for (let y = 0; y < visual_viewbox.height; y++) {
+    for (let x = 0; x < visual_viewbox.width; x++) {
+        const rect = document.createElementNS("http://www.w3.org/2000/svg", 'rect');
+        rect.setAttribute('width', '1');
+        rect.setAttribute('height', '1');
+        rect.setAttribute('x', x);
+        rect.setAttribute('y', y);
+        rect.onclick = (event) => {
+            console.log(event);
+            event.target.classList.toggle('active');
+        }
+        visual.appendChild(rect)
+    }
+}
